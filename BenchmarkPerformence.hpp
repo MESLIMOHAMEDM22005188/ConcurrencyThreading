d9048eeff37e3304bfcf21cf685998592ca80c71
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-05-21 20:58:56 +0200

    Create BenchmarkPerformence.hpp

---
+++ BenchmarkPerformence.hpp
@@ -0,0 +1,10 @@
+#ifndef BENCHMARKPERFORMANCE_HPP
+#define BENCHMARKPERFORMANCE_HPP
+
+namespace BenchmarkPerformance {
+    double runBenchmark();
+    void printPerformance(double seconds);
+}
+
+#endif // BENCHMARKPERFORMANCE_HPP
+
