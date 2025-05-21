95691f1536237641a4e1dd1cf3f5f2a6b8639720
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-05-21 20:57:54 +0200

    Create BarberProblem.hpp

---
+++ BarberProblem.hpp
@@ -0,0 +1,12 @@
+#ifndef BARBER_PROBLEM_HPP
+#define BARBER_PROBLEM_HPP
+
+#include <string>
+
+namespace BarberProblem {
+    void runBarberProblem();
+    std::string generateBarberHTML();
+}
+
+#endif
+
