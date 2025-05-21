300b4506b8a97b9e306cdf535aa4ee65cb70cf2f
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-05-21 21:01:28 +0200

    Create LightouseProblem.hpp

---
+++ LightouseProblem.hpp
@@ -0,0 +1,12 @@
+#ifndef LIGHTHOUSE_PROBLEM_HPP
+#define LIGHTHOUSE_PROBLEM_HPP
+
+#include <string>
+
+namespace LighthouseProblem {
+    void runLighthouseProblem();
+    std::string generateLighthouseHTML();
+}
+
+#endif
+
