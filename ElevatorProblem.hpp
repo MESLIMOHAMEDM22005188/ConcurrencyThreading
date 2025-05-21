ce029928c9ebaa8c84fb5cd7aa27aad3da508005
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-05-21 21:00:37 +0200

    Create ElevatorProblem.hpp

---
+++ ElevatorProblem.hpp
@@ -0,0 +1,12 @@
+#ifndef ELEVATOR_PROBLEM_HPP
+#define ELEVATOR_PROBLEM_HPP
+
+#include <string>
+
+namespace ElevatorProblem {
+    void runElevatorProblem();
+    std::string generateElevatorHTML();
+}
+
+#endif
+
