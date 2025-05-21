bd99ac71b7fefab633c985486258029aadc2e5aa
MESLIMOHAMEDM22005188 <125298121+MESLIMOHAMEDM22005188@users.noreply.github.com>
2025-05-21 20:59:51 +0200

    Create BuffetProblem.hpp

---
+++ BuffetProblem.hpp
@@ -0,0 +1,12 @@
+#ifndef BUFFET_PROBLEM_HPP
+#define BUFFET_PROBLEM_HPP
+
+#include <string>
+
+namespace BuffetProblem {
+    void runBuffetProblem();
+    std::string generateBuffetHTML();
+}
+
+#endif
+
